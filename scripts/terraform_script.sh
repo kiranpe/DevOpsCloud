a5dab0f9942b54ca22ff6f7fdadd8fd5b395ccea
kiranpe <kiranwls2011@gmail.com>
2019-10-17 13:25:19 +0530

    adding more options to terraform script

--- scripts/terraform_script.sh
+++ scripts/terraform_script.sh
@@ -5,14 +5,15 @@ FOLDER="/usr/local/bin"
 while true; do
     echo " "
     echo "Choose option wisely"
-    echo "   all: To run all three terraform commands(Run this setting up first time)"
+    echo "   all: To run all three terraform commands(Run this when setting up first time)"
     echo "   1: To run terraform init command"
     echo "   2: To run terraform plan command"
     echo "   3: To run terraform apply command"
-    echo "   4: To Destroy resource"
-    echo "   5: To refresh terraform state"
-    echo "   6: Show the instance details"
-    echo "   N: Exit from script"
+    echo "   4: To create target resource"
+    echo "   5: To Destroy resource"
+    echo "   6: To refresh terraform state"
+    echo "   7: Show the instance details"
+    echo "   N/n: Exit from script"
     echo " "
 read -p "Select your option from above list: " l
 
@@ -27,18 +28,19 @@ read -p "Select your option from above list: " l
         [2]* ) $FOLDER/terraform plan; break;;
 
         [3]* ) $FOLDER/terraform apply; break;;
-
+        
         [4]* ) read -p "Please enter the target instance to Destroy: " i
+               $FOLDER/terraform apply -target $i; break;;
+
+        [5]* ) read -p "Please enter the target instance to Destroy: " i
                $FOLDER/terraform destroy -target $i; break;;
 
-        [5]* ) $FOLDER/terraform refresh; break;;
-        [6]* ) $FOLDER/terraform show; break;;
+        [6]* ) $FOLDER/terraform refresh; break;;
+
+        [7]* ) $FOLDER/terraform show; break;;
+         
         [Nn]* ) exit;;
+
         * ) echo "Please answer yes or no.";;
     esac
 done
-#/home/build/kp369d/Terraform/terraform init
-
-#/home/build/kp369d/Terraform/terraform plan
-
-#/home/build/kp369d/Terraform/terraform apply

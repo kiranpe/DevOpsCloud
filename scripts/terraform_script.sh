e055264b8d1b95e895a1318655892f78e8eabc76
kiranpe <kiranwls2011@gmail.com>
2019-10-17 13:50:13 +0530

    added few more logics to terraform script

--- scripts/terraform_script.sh
+++ scripts/terraform_script.sh
@@ -2,6 +2,9 @@
 #FOLDER=`pwd` (Use this option when you are using terroform in current folder)
 FOLDER="/usr/local/bin"
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 while true; do
     echo " "
     echo "Choose option wisely"
@@ -9,10 +12,11 @@ while true; do
     echo "   1: To run terraform init command"
     echo "   2: To run terraform plan command"
     echo "   3: To run terraform apply command"
-    echo "   4: To create target resource"
-    echo "   5: To Destroy resource"
-    echo "   6: To refresh terraform state"
-    echo "   7: Show the instance details"
+    echo "   4: To run terraform destroy command"
+    echo "   5: To create target resource with auto approve\n   ${bold}ex: aws_instace.nexus${normal}"
+    echo "   6: To Destroy target resource with auto approve\n   ${bold}ex: aws_instace.nexus${normal}"
+    echo "   7: To refresh terraform state"
+    echo "   8: Show the instance details"
     echo "   N/n: Exit from script"
     echo " "
 read -p "Select your option from above list: " l
@@ -28,16 +32,18 @@ read -p "Select your option from above list: " l
         [2]* ) $FOLDER/terraform plan; break;;
 
         [3]* ) $FOLDER/terraform apply; break;;
+     
+        [4]* ) $FOLDER/terraform destroy; break;;
         
-        [4]* ) read -p "Please enter the target instance to Destroy: " i
-               $FOLDER/terraform apply -target $i; break;;
+        [5]* ) read -p "Please enter the target instance to Create: " i
+               $FOLDER/terraform apply -target $i -auto-approve; break;;
 
-        [5]* ) read -p "Please enter the target instance to Destroy: " i
-               $FOLDER/terraform destroy -target $i; break;;
+        [6]* ) read -p "Please enter the target instance to Destroy: " i
+               $FOLDER/terraform destroy -target $i -auto-approve; break;;
 
-        [6]* ) $FOLDER/terraform refresh; break;;
+        [7]* ) $FOLDER/terraform refresh; break;;
 
-        [7]* ) $FOLDER/terraform show; break;;
+        [8]* ) $FOLDER/terraform show; break;;
          
         [Nn]* ) exit;;
 
